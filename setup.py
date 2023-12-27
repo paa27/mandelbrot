69ce90766f60146a2f463472ea2abbca39c87f78
paarruti <pablo.arrutia@cern.ch>
2023-12-27 21:33:14 +0100

    fast commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mandelbrot',
+    version='1.0.0',
+    author='Your Name',
+    author_email='your@email.com',
+    description='A project for generating Mandelbrot fractals',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'pandas',
+        'numpy',
+        'jupyter',
+        'ipympl'
+    ],
+)
